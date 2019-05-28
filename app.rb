b95fd82acc0a91a9bf73402879a27cbbf13ee35a
Megan Goode <megan.e.goode@hotmail.com>
2019-05-28 16:42:34 +0100

    Add params to named-cat

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/new' do
   "new page"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
   erb(:index)
 end
